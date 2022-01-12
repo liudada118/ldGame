f5ba3f99823b28303fa3f74e900dad0c582e06b4
麦辣鸡腿堡 <oncwnuCB6Xn1PiN5Slc1OQQ_NbU8@git.weixin.qq.com>
2022-01-12 19:00:36 +0800

    联机动画执行
    联机动画执行

--- static/js/util.js
+++ static/js/util.js
@@ -10,7 +10,7 @@ var util = {
     bindAction(keyCodeArr, keyCode, actionCodeArr, actionObj, actionName , aniName) {
         
         if (keyCodeArr.includes(keyCode)) {
-            console.log(keyCodeArr, keyCode, actionCodeArr, actionObj[aniName], actionName)
+            
             if (!actionObj[aniName]) {
                 actionCodeArr.push(actionName)
             }
